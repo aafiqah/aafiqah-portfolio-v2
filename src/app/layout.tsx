a633bdcbb51830113c980453b63c30013fb627be
Alessa Afiqah <alessajohar@gmail.com>
2025-12-18 09:36:55 +0800

    feat (navbar): add navbar liquid glass

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,26 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import Navbar from "@/components/layout/Navbar";
 import { ThemeProvider } from "@/components/theme/theme-provider";
+import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("http://localhost:3000"),
+  title: "Alessa Portfolio",
+  description: "Welcome to My Site",
+  openGraph: {
+    title: "Alessa Portfolio",
+    description: "Welcome to My Site",
+    images: [
+      {
+        url: "/og.png",
+        width: 1200,
+        height: 630,
+        alt: "Alessa Portfolio",
+      },
+    ],
+  },
+};
+
 
 export default function RootLayout({
   children,
@@ -12,6 +31,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className="antialiased">
         <ThemeProvider>
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,10 +2,7 @@ import ThemeToggle from "../theme/theme-toggle";
 
 export default function Hero() {
   return (
-    <section className="min-h-screen flex flex-col justify-center gap-6">
-       <div className="absolute top-6 right-6">
-        <ThemeToggle />
-      </div>
+    <section id="hero" className="min-h-screen flex flex-col justify-center gap-6">
       
       <h1 className="text-4xl md:text-5xl font-bold">
         Hi, I’m <span className="text-blue-500">Alessa Afiqah Johar</span> 👋
